5d47632f8e8aaa40349e87387ccb779337c0ad57
yunxijun <2318085206@qq.com>
2021-08-21 23:41:33 +0800

    添加入口文件

---
+++ app.js
@@ -0,0 +1,14 @@
+// 这是入口文件
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
