136cc6c23c797cf867176482bd94180c9fa4029b
yunxijun <2318085206@qq.com>
2021-09-04 23:26:38 +0800

    change the port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 app.use('/static', express.static('public'))
 
